f5c15afd00ed759240f3d964e7a9f002d65a9e42
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-03-12 08:26:44 -0400

    Added calibrated_data

---
+++ R/calibrated_data.R
@@ -0,0 +1,15 @@
+#' Combining sound and motion into data channels at same times
+#'
+#' @param dat A list, output from `readData`.
+#'
+#' @return A single data.frame/tibble containing 5 columns: time, x, y, z, sound
+#' @export
+#'
+#' @examples
+calibrated_data <- function(dat){
+  sound <- dat$Sound
+  motion <- dat$Motion
+  bl = motion
+  bl$sound = approx(sound$Time, sound$Sound, xout = motion$Time)$y
+  return(bl)
+}
---
+++ R/zdeviceR-package.R
@@ -0,0 +1,11 @@
+#' zdeviceR
+#'
+#' Loading and processing data from zdevice
+#'
+#' @docType package
+#' @author Abhijit Dasgupta
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib zdeviceR
+#' @name zdeviceR
+NULL
