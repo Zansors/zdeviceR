0ecef19d4d2862d43ddca96088824d400778bdb3
Abhijit Dasgupta <aikidasgupta@gmail.com>
2019-07-15 15:08:56 -0400

    fixed plt_resp_var to make sure plot printed

--- R/plot_resp_var.R
+++ R/plot_resp_var.R
@@ -8,9 +8,10 @@
 #' @examples
 plot_resp_var <- function(d){
   require(ggplot2)
-  plt <- ggplot(resp_variability) + geom_line(aes(mins, respiration_rate)) +
+  plt <- ggplot(d) + geom_line(aes(mins, respiration_rate)) +
     geom_line(aes(mins, lcb), color='red') +
     geom_line(aes(mins, ucb), color = 'red') +
     # ylim(0,80) +
     labs(x = 'Minutes', y = 'Breathing rate')
+  print(plt)
 }
